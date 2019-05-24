905f55aaa50cceae9bc102ff39f2178e23a454fe
pondahai <dahai.pon@gmail.com>
2019-05-24 23:36:09 +0800

    create others.js

---
+++ js/others.js
@@ -0,0 +1,11 @@
+  'use strict';
+  downloadCurrentDocument = function () {
+
+    var base64doc = btoa(unescape(encodeURIComponent(document.documentElement.outerHTML))),
+        a = document.createElement('a'),
+        e = new MouseEvent('click');
+
+    a.download = 'doc.html';
+    a.href = 'data:text/html;base64,' + base64doc;
+    a.dispatchEvent(e);
+  };
