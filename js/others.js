834d7fd2b0802a82190e212247df8c91eb0e9d4c
pondahai <dahai.pon@gmail.com>
2019-05-27 01:11:59 +0800

    Update others.js

--- js/others.js
+++ js/others.js
@@ -122,7 +122,7 @@
 		form.append('file', file);
 
 		var xhr = new XMLHttpRequest();
-		xhr.open('post', 'https://www.googleapis.com/upload/drive/v3/files?uploadType=media');
+		xhr.open('post', 'https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart&fields=id');
 		xhr.setRequestHeader('Authorization', 'Bearer ' + accessToken);
 		xhr.responseType = 'json';
 		xhr.onload = () => {
