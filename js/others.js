90a2a09251372b665beb2114e71b38570f76e62c
pondahai <dahai.pon@gmail.com>
2019-05-31 21:55:10 +0800

    minor

--- js/others.js
+++ js/others.js
@@ -536,13 +536,13 @@
           savecloudfileButton.style.display = '';
           shareFileButton.style.display = '';
           listFiles();
-  var profile = gapi.auth2.currentUser.get().getBasicProfile();
-  console.log('ID: ' + profile.getId());
-  console.log('Full Name: ' + profile.getName());
-  console.log('Given Name: ' + profile.getGivenName());
-  console.log('Family Name: ' + profile.getFamilyName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail());
+  // var profile = gapi.auth2.currentUser.get().getBasicProfile();
+  // console.log('ID: ' + profile.getId());
+  // console.log('Full Name: ' + profile.getName());
+  // console.log('Given Name: ' + profile.getGivenName());
+  // console.log('Family Name: ' + profile.getFamilyName());
+  // console.log('Image URL: ' + profile.getImageUrl());
+  // console.log('Email: ' + profile.getEmail());
         } else {
           authorizeButton.style.display = '';
           signoutButton.style.display = 'none';
