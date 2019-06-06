5d166685a4e3b0d1a42a948e3159f2c6f1391f3a
dahai <dahai.pon@gmail.com>
2019-06-06 18:08:02 +0800

    minor

--- js/others.js
+++ js/others.js
@@ -426,7 +426,7 @@
 		fbShareButtonSvg.setAttributeNS("http://www.w3.org/2000/svg", 'width', '30');
 		fbShareButtonSvg.setAttributeNS("http://www.w3.org/2000/svg", 'height', '30');
 		fbShareButtonSvg.setAttributeNS("http://www.w3.org/2000/svg", 'version', '1.1');
-		fbShareButtonSvg.setAttributeNS("http://www.w3.org/2000/svg", 'xmlns', 'http://www.w3.org/2000/svg');
+		// fbShareButtonSvg.setAttributeNS("http://www.w3.org/2000/svg", 'xmlns', 'http://www.w3.org/2000/svg');
 		fbShareButtonSvg.setAttributeNS("http://www.w3.org/2000/svg", 'xmlns:xlink', 'http://www.w3.org/1999/xlink');
 		// var fbShareButton = document.createElement('a');
 		// fbShareButton.setAttribute('id','idFBshareButton');
