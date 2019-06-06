4fb6a1338a92a15492078e5fdde5f288e0cf0339
dahai <dahai.pon@gmail.com>
2019-06-06 16:14:13 +0800

    minor

--- js/others.js
+++ js/others.js
@@ -412,10 +412,10 @@
 		svgRect.setAttribute('height','30');
 		svgRect.setAttribute('fill-opacity','0');
 		svgRect.setAttribute('stroke-opacity','0');
-		svgRect.setAttributeNS("http://www.w3.org/2000/svg", 'xlink:href', 'fbshareCurrentPage("'+url_encoded+'","'+name_encoded+'")');
+		svgRect.setAttributeNS("http://www.w3.org/2000/svg", 'onclick', 'fbshareCurrentPage("'+url_encoded+'","'+name_encoded+'")');
 		var fbShareButtonSvg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
 		fbShareButtonSvg.appendChild(svgRect);
-		fbShareButtonSvg.appendChild(fbShareButton);
+		//fbShareButtonSvg.appendChild(fbShareButton);
 		document.getElementById('fbsharebuttonposition').appendChild(fbShareButtonSvg);
 		// fbShareButtonSvg.setAttribute('viewBox', '0 0 100 100');
 		fbShareButtonSvg.setAttribute('id', 'idFBshareButton');
