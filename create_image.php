62b2514022578d534deb5067d42307d8c6756400
pondahai <dahai.pon@gmail.com>
2019-07-11 23:03:53 +0800

    Update create_image.php

--- create_image.php
+++ create_image.php
@@ -4,29 +4,34 @@ $url = $_GET['url'];
 //website url
 $siteURL = $url;
 
-//call Google PageSpeed Insights API
-$googlePagespeedData = file_get_contents("https://www.googleapis.com/pagespeedonline/v2/runPagespeed?url=$siteURL&screenshot=true");
 
-//decode json data
-$googlePagespeedData = json_decode($googlePagespeedData, true);
 
-//screenshot data
-$screenshot = $googlePagespeedData['screenshot']['data'];
-$screenshot = str_replace(array('_','-'),array('/','+'),$screenshot); 
+// //call Google PageSpeed Insights API
+// $googlePagespeedData = file_get_contents("https://www.googleapis.com/pagespeedonline/v2/runPagespeed?url=$siteURL&screenshot=true");
+
+// //decode json data
+// $googlePagespeedData = json_decode($googlePagespeedData, true);
+
+// //screenshot data
+// $screenshot = $googlePagespeedData['screenshot']['data'];
+// $screenshot = str_replace(array('_','-'),array('/','+'),$screenshot); 
+
+
 
 $dom = new DOMDocument();
 $dom->loadHTML(file_get_contents($siteURL));
 $searchNode = $dom->getElementsByTagName( "image" ); 
 foreach( $searchNode as $searchNode ) 
 {
-    $data = $searchNode->getAttribute( "xlink:href" ); 
+    $screenshot = $searchNode->getAttribute( "xlink:href" ); 
     break;
 }
+$screenshot = str_replace(array('_','-'),array('/','+'),$screenshot); 
 
 //display screenshot image
 //echo "<img src=\"data:image/jpeg;base64,".$screenshot."\" />";
 header("Content-type: image/png");
-//$data = base64_decode($screenshot);
+$data = base64_decode($screenshot);
 $source = imagecreatefromstring($data);
 $thumb = imagecreatetruecolor(400, 400);
 imagecopyresized($thumb, $source, 0, 0, 0, 0, 400, 400, imagesx($source), imagesy($source));
