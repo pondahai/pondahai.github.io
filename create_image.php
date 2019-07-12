b58c0e57079fe12473bf486b027509c79a417555
dahai <dahai.pon@gmail.com>
2019-07-12 09:26:42 +0800

    Update create_image.php

--- create_image.php
+++ create_image.php
@@ -21,7 +21,7 @@ $siteURL = $url;
 $dom = new DOMDocument();
 @$dom->loadHTML(file_get_contents($siteURL));
 $searchNode = $dom->getElementsByTagName( "image" );
-if ($searchNode->length == 0)) { 
+if ($searchNode->length == 0) { 
 	$source = imagecreatefrompng('icon01.png'); 
 }else{
 	foreach( $searchNode as $searchNode ) 
