06833c9150cce5c30779b78dfc234c061cbab2b7
dahai <dahai.pon@gmail.com>
2019-08-30 16:39:13 +0800

    minor

--- simple_html_dom.php
+++ simple_html_dom.php
@@ -62,7 +62,7 @@ define('HDOM_INFO_ENDSPACE',7);
 define('DEFAULT_TARGET_CHARSET', 'UTF-8');
 define('DEFAULT_BR_TEXT', "\r\n");
 define('DEFAULT_SPAN_TEXT', " ");
-define('MAX_FILE_SIZE', 60000000);
+define('MAX_FILE_SIZE', 10000000);
 // helper functions
 // -----------------------------------------------------------------------------
 // get html dom from file
