c332a122ccf4deb2f4bf10716437b218dfa34cc2
dahai <dahai.pon@gmail.com>
2019-09-17 10:03:09 +0800

    Update quickstart.php

--- quickstart.php
+++ quickstart.php
@@ -2,6 +2,18 @@
 
         //$fileId = '0BwwA4oUTeiV1UVNwOHItT0xfa2M';
         if(isset($content)) {
-            print $content;
+            // print $content;
+                $find_first_element = $html->find('*', 0);
+                if(is_object($find_first_element)){
+                    print $find_first_element;
+                }
+                $find_secend_element = $html->find('*', 1);
+                if(is_object($find_secend_element)){
+                    print $find_secend_element;
+                }
+                $find_third_element = $html->find('*', 2);
+                if(is_object($find_third_element)){
+                    print $find_third_element;
+                }
         }
 ?>
