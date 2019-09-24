a02210c32b2ccb8f18067031082991ab6e439bdb
pondahai <dahai.pon@gmail.com>
2019-09-25 00:27:11 +0800

    Update quickstart.php

--- quickstart.php
+++ quickstart.php
@@ -1,29 +1,52 @@
 <?php 
 
-    function closetags($html) {
+function truncateHTML($html, $length)
+{
+    $truncatedText = substr($html, $length);
+    $pos = strpos($truncatedText, ">");
+    if($pos !== false)
+    {
+        $html = substr($html, 0,$length + $pos + 1);
+    }
+    else
+    {
+        $html = substr($html, 0,$length);
+    }
+
     preg_match_all('#<(?!meta|img|br|hr|input\b)\b([a-z]+)(?: .*)?(?<![/|/ ])>#iU', $html, $result);
     $openedtags = $result[1];
+
     preg_match_all('#</([a-z]+)>#iU', $html, $result);
     $closedtags = $result[1];
+
     $len_opened = count($openedtags);
-    if (count($closedtags) == $len_opened) {
+
+    if (count($closedtags) == $len_opened)
+    {
         return $html;
     }
+
     $openedtags = array_reverse($openedtags);
-    for ($i=0; $i < $len_opened; $i++) {
-        if (!in_array($openedtags[$i], $closedtags)) {
+    for ($i=0; $i < $len_opened; $i++)
+    {
+        if (!in_array($openedtags[$i], $closedtags))
+        {
             $html .= '</'.$openedtags[$i].'>';
-        } else {
+        }
+        else
+        {
             unset($closedtags[array_search($openedtags[$i], $closedtags)]);
         }
     }
+
+
     return $html;
-} 
+}
         //$fileId = '0BwwA4oUTeiV1UVNwOHItT0xfa2M';
         if(isset($fileid) and isset($http) and isset($content) and isset($html) and is_object($html)) {
         // print $content;
             foreach($html->find('*') as $element)
-                echo closetags($element);
+                echo truncateHTML($element, strlen($element));
                 
 
             // $find_first_element = $html->find('*', 0);
