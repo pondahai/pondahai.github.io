0a4c031cf4440099332f3e2981a963aeb8071dc1
dahai <dahai.pon@gmail.com>
2019-09-25 12:44:05 +0800

    Update full.php

--- full.php
+++ full.php
@@ -108,12 +108,13 @@
         $chunkStart = $chunkEnd + 1;
         $content = $response->getBody()->getContents();
         //$html = str_get_html($content);
-        if ($response->getStatusCode() != 200 or $response->getStatusCode() != 204 or $response->getStatusCode() != 206) {
-            http_response_code($response->getStatusCode());
-        }else{ 
+        if ($response->getStatusCode() == 200 or $response->getStatusCode() == 204 or $response->getStatusCode() == 206) {
             if (isset($content)) {
                 echo $content;
-        }}
+            }
+        }else{ 
+            http_response_code($response->getStatusCode());
+        }
 	}
 
    	// if(isset($fileid) and isset($driveService)) {
